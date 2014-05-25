da539359f139126b5c1d565db40ae867a423057a
Thomas Levesque <tom103@free.fr>
2014-05-26 01:30:33 +0200

    Added TakeEvery, SkipEvery, Batch Renamed partial class files

---
+++ Linq.Extras/Batch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Linq.Extras.Internal;
+
+namespace Linq.Extras
+{
+    partial class EnumerableEx
+    {
+        public static IEnumerable<IEnumerable<TSource>> Batch<TSource>(
+            [NotNull] this IEnumerable<TSource> source,
+            int count)
+        {
+            source.CheckArgumentNull("source");
+            count.CheckArgumentOutOfRange("count", 1, int.MaxValue);
+
+            return source
+                .WithIndex()
+                .GroupBy(x => x.Index / count)
+                .Select(g => g.WithoutIndex());
+        }
+    }
+}
---
+++ Linq.Extras/TakeEvery.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Linq.Extras.Internal;
+
+namespace Linq.Extras
+{
+    partial class EnumerableEx
+    {
+        public static IEnumerable<TSource> TakeEvery<TSource>(
+            [NotNull] this IEnumerable<TSource> source,
+            int frequency)
+        {
+            return source.TakeEvery(frequency, 0);
+        }
+
+        public static IEnumerable<TSource> TakeEvery<TSource>(
+            [NotNull] this IEnumerable<TSource> source,
+            int frequency,
+            int start)
+        {
+            source.CheckArgumentNull("source");
+            frequency.CheckArgumentOutOfRange("frequency", 1, int.MaxValue);
+            start.CheckArgumentOutOfRange("start", 0, int.MaxValue);
+
+            return source
+                .Skip(start)
+                .Where((item, index) => index % frequency == 0);
+        }
+    }
+}
