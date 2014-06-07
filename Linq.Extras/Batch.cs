f7699979b152e9d6952bf5730192f6a901681db7
Thomas Levesque <tom103@free.fr>
2014-06-07 02:48:09 +0200

    Added documentation comments for Batch

--- Linq.Extras/Batch.cs
+++ Linq.Extras/Batch.cs
@@ -7,6 +7,13 @@ namespace Linq.Extras
 {
     partial class XEnumerable
     {
+        /// <summary>
+        /// Splits the input sequence into a sequence of batches of the specified size.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to split into batches.</param>
+        /// <param name="size">The size of the batches</param>
+        /// <returns>A sequence of batches of the specified size; the last batch can be shorter if there isn't enough elements remaining in the input sequence.</returns>
         [Pure]
         public static IEnumerable<IEnumerable<TSource>> Batch<TSource>(
             [NotNull] this IEnumerable<TSource> source,
