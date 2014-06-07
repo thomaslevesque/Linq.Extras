f8b16d527046b1ba20b3c295f766b29da67ed9eb
Thomas Levesque <tom103@free.fr>
2014-06-07 16:04:28 +0200

    Added tests and documentation for ExceptBy

---
+++ Linq.Extras.Tests/XEnumerableTests/ExceptByTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Linq.Extras.Tests.XEnumerableTests
+{
+    [TestFixture]
+    class ExceptByTests
+    {
+        [Test]
+        public void ExceptBy_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            IEnumerable<int> other = Enumerable.Empty<int>();
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => source.ExceptBy(other, Math.Abs));
+            ex.ParamName.Should().Be("source");
+        }
+
+        [Test]
+        public void ExceptBy_Throws_If_Other_Is_Null()
+        {
+            IEnumerable<int> source = Enumerable.Empty<int>();
+            IEnumerable<int> other = null;
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => source.ExceptBy(other, Math.Abs));
+            ex.ParamName.Should().Be("other");
+        }
+
+        [Test]
+        public void ExceptBy_Throws_If_KeySelector_Is_Null()
+        {
+            var source = Enumerable.Empty<int>();
+            var other = Enumerable.Empty<int>();
+            Func<int, string> keySelector = null;
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => source.ExceptBy(other, keySelector));
+            ex.ParamName.Should().Be("keySelector");
+        }
+
+        [Test]
+        public void ExceptBy_Returns_Items_From_Source_Not_In_Other_Based_On_The_Key()
+        {
+            var source = new[]
+                         {
+                             new Foo(0, 1),
+                             new Foo(0, 2),
+                             new Foo(1, 3),
+                             new Foo(2, 5),
+                             new Foo(2, 0),
+                             new Foo(3, 2)
+                         };
+            var other = new[]
+                        {
+                            new Foo(0, 7),
+                            new Foo(1, 0),
+                        };
+            var result = source.ExceptBy(other, f => f.X);
+            result.Should().BeEquivalentTo(
+                new[]
+                {
+                    new Foo(2, 5),
+                    new Foo(3, 2)
+                });
+        }
+
+        [Test]
+        public void ExceptBy_Uses_The_Specified_Key_Comparer()
+        {
+            var source = new[]
+                         {
+                             new Foo(0, 1),
+                             new Foo(0, 2),
+                             new Foo(1, 3),
+                             new Foo(-2, 5),
+                             new Foo(-2, 0),
+                             new Foo(3, 2)
+                         };
+            var others = new[]
+                         {
+                             new Foo(2, 5),
+                             new Foo(-3, 2)
+                         };
+            var comparer = XEqualityComparer<int>.By(Math.Abs);
+            var result = source.ExceptBy(others, f => f.X, comparer);
+            result.Should().BeEquivalentTo(
+                new[]
+                {
+                    new Foo(0, 1),
+                    new Foo(1, 3),
+                });
+        }
+
+        [ExcludeFromCodeCoverage]
+        struct Foo
+        {
+            public Foo(int x, int y) : this()
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; private set; }
+            // ReSharper disable once MemberCanBePrivate.Local
+            // ReSharper disable once UnusedAutoPropertyAccessor.Local
+            public int Y { get; private set; }
+        }
+    }
+}
--- Linq.Extras/ExceptBy.cs
+++ Linq.Extras/ExceptBy.cs
@@ -8,21 +8,22 @@ namespace Linq.Extras
 {
     partial class XEnumerable
     {
-        [Pure]
-        public static IEnumerable<TSource> ExceptBy<TSource, TKey>(
-            [NotNull] this IEnumerable<TSource> source,
-            [NotNull] IEnumerable<TSource> other,
-            Func<TSource, TKey> keySelector)
-        {
-            return source.ExceptBy(other, keySelector, null);
-        }
-
+        /// <summary>
+        /// Produces the set difference of two sequences by using the specified key and key comparer to test for equality between elements.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of the input sequences.</typeparam>
+        /// <typeparam name="TKey">The type of the key used for testing equality between elements.</typeparam>
+        /// <param name="source">A sequence whose elements that are not also in second will be returned.</param>
+        /// <param name="other">A sequence whose elements that also occur in the first sequence will cause those elements to be removed from the returned sequence.</param>
+        /// <param name="keySelector">A delegate that returns the key used to test equality between elements.</param>
+        /// <param name="keyComparer">A comparer used to test equality between keys (can be null).</param>
+        /// <returns>A sequence that contains the set difference of the elements of two sequences.</returns>
         [Pure]
         public static IEnumerable<TSource> ExceptBy<TSource, TKey>(
             [NotNull] this IEnumerable<TSource> source,
             [NotNull] IEnumerable<TSource> other,
             [NotNull] Func<TSource, TKey> keySelector,
-            IEqualityComparer<TKey> keyComparer)
+            IEqualityComparer<TKey> keyComparer = null)
         {
             source.CheckArgumentNull("source");
             other.CheckArgumentNull("other");
