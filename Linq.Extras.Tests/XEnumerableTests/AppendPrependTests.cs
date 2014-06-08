af94b92d9dbbc4e02569a599a7dcfed40694903d
Thomas Levesque <tom103@free.fr>
2014-06-08 18:07:55 +0200

    Completed tests for Append and Prepend

--- Linq.Extras.Tests/XEnumerableTests/AppendPrependTests.cs
+++ Linq.Extras.Tests/XEnumerableTests/AppendPrependTests.cs
@@ -1,10 +1,23 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Linq.Extras.Tests.XEnumerableTests
 {
     [TestFixture]
     class AppendPrependTests
     {
+        [Test]
+        public void Append_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            // ReSharper disable once AssignNullToNotNullAttribute
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            var ex = Assert.Throws<ArgumentNullException>(() => source.Append(42));
+            ex.ParamName.Should().Be("source");
+        }
+
         [Test]
         public void Append_Adds_Item_At_End_Of_Sequence()
         {
@@ -13,6 +26,16 @@ namespace Linq.Extras.Tests.XEnumerableTests
             var expected = new[] { 4, 8, 15, 16, 23, 42 };
             var actual = input.Append(item);
             CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Prepend_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            // ReSharper disable once AssignNullToNotNullAttribute
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            var ex = Assert.Throws<ArgumentNullException>(() => source.Prepend(42));
+            ex.ParamName.Should().Be("source");
         }
 
         [Test]
