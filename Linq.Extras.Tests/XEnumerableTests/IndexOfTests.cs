33f7403b95d95e208374ac8225289a6f3d138659
Thomas Levesque <tom103@free.fr>
2014-06-08 15:54:09 +0200

    Added tests and documentation for IndexOf

---
+++ Linq.Extras.Tests/XEnumerableTests/IndexOfTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Linq.Extras.Tests.XEnumerableTests
+{
+    [TestFixture]
+    class IndexOfTests
+    {
+        [Test]
+        public void IndexOf_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => source.IndexOf(42));
+            ex.ParamName.Should().Be("source");
+        }
+
+        [Test]
+        public void IndexOf_Returns_MinusOne_If_Element_Is_Not_Found()
+        {
+            var source = new[] { 4, 8, 15, 16, 23, 42 };
+            int index = source.IndexOf(99);
+            index.Should().Be(-1);
+        }
+
+        [Test]
+        public void IndexOf_Returns_Index_Of_First_Element_If_Found()
+        {
+            var source = new[] { 4, 8, 15, 16, 23, 42, 4, 8 , 15, 16, 23, 42 };
+            int index = source.IndexOf(42);
+            index.Should().Be(5);
+        }
+
+        [Test]
+        public void IndexOf_Throws_If_Predicate_Is_Null()
+        {
+            var source = Enumerable.Empty<int>();
+            Func<int, bool> predicate = null;
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => source.IndexOf(predicate));
+            ex.ParamName.Should().Be("predicate");
+        }
+
+        [Test]
+        public void IndexOf_Returns_MinusOne_If_No_Element_Matches_Predicate()
+        {
+            var source = new[] { 4, 8, 15, 16, 23, 42 };
+            int index = source.IndexOf(i => i % 12 == 0);
+            index.Should().Be(-1);
+        }
+
+        [Test]
+        public void IndexOf_Returns_Index_Of_First_Element_That_Matches_Predicate()
+        {
+            var source = new[] { 4, 8, 15, 16, 23, 42, 4, 8, 15, 16, 23, 42 };
+            int index = source.IndexOf(i => i % 7 == 0);
+            index.Should().Be(5);
+        }
+
+        [Test]
+        public void LastIndexOf_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => source.LastIndexOf(42));
+            ex.ParamName.Should().Be("source");
+        }
+
+        [Test]
+        public void LastIndexOf_Returns_MinusOne_If_Element_Is_Not_Found()
+        {
+            var source = new[] { 4, 8, 15, 16, 23, 42 };
+            int index = source.LastIndexOf(99);
+            index.Should().Be(-1);
+        }
+
+        [Test]
+        public void LastIndexOf_Returns_Index_Of_Last_Element_If_Found()
+        {
+            var source = new[] { 4, 8, 15, 16, 23, 42, 4, 8, 15, 16, 23, 42 };
+            int index = source.LastIndexOf(42);
+            index.Should().Be(11);
+        }
+
+        [Test]
+        public void LastIndexOf_Throws_If_Predicate_Is_Null()
+        {
+            var source = Enumerable.Empty<int>();
+            Func<int, bool> predicate = null;
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => source.LastIndexOf(predicate));
+            ex.ParamName.Should().Be("predicate");
+        }
+
+        [Test]
+        public void LastIndexOf_Returns_MinusOne_If_No_Element_Matches_Predicate()
+        {
+            var source = new[] { 4, 8, 15, 16, 23, 42 };
+            int index = source.LastIndexOf(i => i % 12 == 0);
+            index.Should().Be(-1);
+        }
+
+        [Test]
+        public void LastIndexOf_Returns_Index_Of_Last_Element_That_Matches_Predicate()
+        {
+            var source = new[] { 4, 8, 15, 16, 23, 42, 4, 8, 15, 16, 23, 42 };
+            int index = source.LastIndexOf(i => i % 7 == 0);
+            index.Should().Be(11);
+        }
+    }
+}
--- Linq.Extras/IndexOf.cs
+++ Linq.Extras/IndexOf.cs
@@ -8,48 +8,66 @@ namespace Linq.Extras
 {
     partial class XEnumerable
     {
+        /// <summary>
+        /// Returns the position of the first occurrence of <c>item</c> in the <c>source</c> sequence, or -1 if it is not found.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to search for <c>item</c>.</param>
+        /// <param name="item">The element to find the index of.</param>
+        /// <param name="comparer">The comparer to use to test for equality between elements.</param>
+        /// <returns>The zero-based index of the first occurrence of <c>item</c> if it is found; otherwise, -1.</returns>
         [Pure]
-        public static int IndexOf<TSource>([NotNull] this IEnumerable<TSource> source, TSource item)
-        {
-            return source.IndexOf(item, null);
-        }
-
-        [Pure]
-        public static int IndexOf<TSource>([NotNull] this IEnumerable<TSource> source, TSource item, IEqualityComparer<TSource> comparer)
+        public static int IndexOf<TSource>([NotNull] this IEnumerable<TSource> source, TSource item, IEqualityComparer<TSource> comparer = null)
         {
             comparer = comparer ?? EqualityComparer<TSource>.Default;
             return source.IndexOf(i => comparer.Equals(i, item));
         }
 
+        /// <summary>
+        /// Returns the position of the first element of <c>source</c> that verifies the specified predicate, or -1 if it is not found.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to search for a matching element.</param>
+        /// <param name="predicate">The predicate that is tested against each element.</param>
+        /// <returns>The zero-based index of the first element that verifies the predicate, or -1 if no element verifies the predicate.</returns>
         [Pure]
         public static int IndexOf<TSource>([NotNull] this IEnumerable<TSource> source, [NotNull] Func<TSource, bool> predicate)
         {
             source.CheckArgumentNull("source");
-            predicate.CheckArgumentNull("source");
+            predicate.CheckArgumentNull("predicate");
             return source.WithIndex()
                 .Where(i => predicate(i.Value))
                 .Select(i => i.Index)
                 .FirstOrDefault(-1);
         }
 
+        /// <summary>
+        /// Returns the position of the last occurrence of <c>item</c> in the <c>source</c> sequence, or -1 if it is not found.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to search for <c>item</c>.</param>
+        /// <param name="item">The element to find the index of.</param>
+        /// <param name="comparer">The comparer to use to test for equality between elements.</param>
+        /// <returns>The zero-based index of the last occurrence of <c>item</c> if it is found; otherwise, -1.</returns>
         [Pure]
-        public static int LastIndexOf<TSource>([NotNull] this IEnumerable<TSource> source, TSource item)
-        {
-            return source.LastIndexOf(item, null);
-        }
-
-        [Pure]
-        public static int LastIndexOf<TSource>([NotNull] this IEnumerable<TSource> source, TSource item, IEqualityComparer<TSource> comparer)
+        public static int LastIndexOf<TSource>([NotNull] this IEnumerable<TSource> source, TSource item, IEqualityComparer<TSource> comparer = null)
         {
             comparer = comparer ?? EqualityComparer<TSource>.Default;
             return source.LastIndexOf(i => comparer.Equals(i, item));
         }
 
+        /// <summary>
+        /// Returns the position of the last element of <c>source</c> that verifies the specified predicate, or -1 if it is not found.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to search for a matching element.</param>
+        /// <param name="predicate">The predicate that is tested against each element.</param>
+        /// <returns>The zero-based index of the last element that verifies the predicate, or -1 if no element verifies the predicate.</returns>
         [Pure]
         public static int LastIndexOf<TSource>([NotNull] this IEnumerable<TSource> source, [NotNull] Func<TSource, bool> predicate)
         {
             source.CheckArgumentNull("source");
-            predicate.CheckArgumentNull("source");
+            predicate.CheckArgumentNull("predicate");
             return source.WithIndex()
                 .Where(i => predicate(i.Value))
                 .Select(i => i.Index)
