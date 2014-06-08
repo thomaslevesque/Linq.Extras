c8a2f7023e37b825003ff3e46cefde30d6701c9f
Thomas Levesque <tom103@free.fr>
2014-06-08 04:07:27 +0200

    Added tests and documentation for Generate

---
+++ Linq.Extras.Tests/XEnumerableTests/GenerateTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Linq.Extras.Tests.XEnumerableTests
+{
+    [TestFixture]
+    class GenerateTests
+    {
+        [Test]
+        public void Generate_Throws_If_Generator_Is_Null()
+        {
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => XEnumerable.Generate(0, null));
+            ex.ParamName.Should().Be("generator");
+        }
+
+        [Test]
+        public void Generate_By_Index_Throws_If_Generator_Is_Null()
+        {
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var ex = Assert.Throws<ArgumentNullException>(() => XEnumerable.Generate<int>(null));
+            ex.ParamName.Should().Be("generator");
+        }
+
+        [Test]
+        public void Generate_Returns_Sequence_Of_Values_Based_On_Previous_Value()
+        {
+            var sequence = XEnumerable.Generate(0, previous => previous + 2);
+            var expected = new[] { 0, 2, 4, 6, 8 };
+            var actual = sequence.Take(5);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void Generate_By_Index_Returns_Sequence_Of_Values_Based_On_Index()
+        {
+            var sequence = XEnumerable.Generate(index => index * 2);
+            var expected = new[] { 0, 2, 4, 6, 8 };
+            var actual = sequence.Take(5);
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
--- Linq.Extras/Generate.cs
+++ Linq.Extras/Generate.cs
@@ -7,38 +7,51 @@ namespace Linq.Extras
 {
     partial class XEnumerable
     {
+        /// <summary>
+        /// Generates a sequence from a seed and a generator function.
+        /// </summary>
+        /// <typeparam name="TElement">The type of the elements in the sequence.</typeparam>
+        /// <param name="seed">The first element of the sequence.</param>
+        /// <param name="generator">A generator function that returns the next element from the current element.</param>
+        /// <returns>An infinite sequence of generated elements.</returns>
         [Pure]
-        public static IEnumerable<TSource> Generate<TSource>(TSource seed, [NotNull] Func<TSource, TSource> nextSelector)
+        public static IEnumerable<TElement> Generate<TElement>(TElement seed, [NotNull] Func<TElement, TElement> generator)
         {
-            nextSelector.CheckArgumentNull("nextSelector");
-            return GenerateIterator(seed, nextSelector);
+            generator.CheckArgumentNull("generator");
+            return GenerateIterator(seed, generator);
         }
 
-        private static IEnumerable<TSource> GenerateIterator<TSource>(TSource seed, Func<TSource, TSource> nextSelector)
+        private static IEnumerable<TElement> GenerateIterator<TElement>(TElement seed, Func<TElement, TElement> generator)
         {
-            TSource current = seed;
+            TElement current = seed;
             while (true)
             {
                 yield return current;
-                current = nextSelector(current);
+                current = generator(current);
             }
             // ReSharper disable once FunctionNeverReturns
         }
 
+        /// <summary>
+        /// Generates a sequence from an index-based generator function.
+        /// </summary>
+        /// <typeparam name="TElement">The type of the elements in the sequence.</typeparam>
+        /// <param name="generator">A generator function that returns an element based on its index.</param>
+        /// <returns>An sequence of elements generated from their index.</returns>
+        /// <remarks>The index varies from 0 (inclusive) to <c>int.MaxValue</c> (exclusive), so the output sequence contains <c>int.MaxValue</c> elements.</remarks>
         [Pure]
-        public static IEnumerable<TSource> Generate<TSource>([NotNull] Func<int, TSource> generator)
+        public static IEnumerable<TElement> Generate<TElement>([NotNull] Func<int, TElement> generator)
         {
             generator.CheckArgumentNull("generator");
             return GenerateIterator(generator);
         }
 
-        private static IEnumerable<TSource> GenerateIterator<TSource>(Func<int, TSource> generator)
+        private static IEnumerable<TElement> GenerateIterator<TElement>(Func<int, TElement> generator)
         {
             for (int i = 0; i < int.MaxValue; i++)
             {
                 yield return generator(i);
             }
-            yield return generator(int.MaxValue);
         }
     }
 }
