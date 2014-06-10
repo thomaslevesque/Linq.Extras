fbc830b8c84efb01b89b09681493e16d00f1ac7d
Thomas Levesque <tom103@free.fr>
2014-06-11 01:53:06 +0200

    Added tests and documentation for ToQueue, ToStack, ToHashSet and ToLinkedList

---
+++ Linq.Extras.Tests/XEnumerableTests/ToCollectionsTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Linq.Extras.Tests.XEnumerableTests
+{
+    [TestFixture]
+    class ToCollectionsTests
+    {
+        [Test]
+        public void ToQueue_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            // ReSharper disable once AssignNullToNotNullAttribute
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            var ex = Assert.Throws<ArgumentNullException>(() => source.ToQueue());
+            ex.ParamName.Should().Be("source");
+        }
+
+        [Test]
+        public void ToQueue_Returns_A_Queue_With_The_Same_Items_As_Source()
+        {
+            var items = new[] { 4, 8, 15, 16, 23, 42 };
+            var source = items.ForbidMultipleEnumeration();
+            var queue = source.ToQueue();
+            queue.Should().Equal(items);
+        }
+
+        [Test]
+        public void ToStack_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            // ReSharper disable once AssignNullToNotNullAttribute
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            var ex = Assert.Throws<ArgumentNullException>(() => source.ToStack());
+            ex.ParamName.Should().Be("source");
+        }
+
+        [Test]
+        public void ToStack_Returns_A_Stack_With_The_Same_Items_As_Source()
+        {
+            var items = new[] { 4, 8, 15, 16, 23, 42 };
+            var source = items.ForbidMultipleEnumeration();
+            var stack = source.ToStack();
+            stack.Should().Equal(items.Reverse());
+        }
+
+        [Test]
+        public void ToHashSet_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            // ReSharper disable once AssignNullToNotNullAttribute
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            var ex = Assert.Throws<ArgumentNullException>(() => source.ToHashSet());
+            ex.ParamName.Should().Be("source");
+        }
+
+        [Test]
+        public void ToHashSet_Returns_A_HashSet_With_The_Same_Items_As_Source()
+        {
+            var items = new[] { 4, 8, 15, 16, 23, 42 };
+            var source = items.ForbidMultipleEnumeration();
+            var hashSet = source.ToHashSet();
+            hashSet.Should().Equal(items);
+        }
+
+        [Test]
+        public void ToHashSet_Uses_The_Provided_Comparer()
+        {
+            var items = new[] { 4, -8, 15, 16, -23, 42 };
+            var source = items.ForbidMultipleEnumeration();
+            var comparer = XEqualityComparer<int>.By(Math.Abs);
+            var hashSet = source.ToHashSet(comparer);
+            hashSet.Should().Equal(items, comparer.Equals);
+        }
+
+        [Test]
+        public void ToLinkedList_Throws_If_Source_Is_Null()
+        {
+            IEnumerable<int> source = null;
+            // ReSharper disable once AssignNullToNotNullAttribute
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            var ex = Assert.Throws<ArgumentNullException>(() => source.ToLinkedList());
+            ex.ParamName.Should().Be("source");
+        }
+
+        [Test]
+        public void ToLinkedList_Returns_A_LinkedList_With_The_Same_Items_As_Source()
+        {
+            var items = new[] { 4, 8, 15, 16, 23, 42 };
+            var source = items.ForbidMultipleEnumeration();
+            var linkedList = source.ToLinkedList();
+            linkedList.Should().Equal(items);
+        }
+    }
+}
--- Linq.Extras/ToCollections.cs
+++ Linq.Extras/ToCollections.cs
@@ -6,29 +6,54 @@ namespace Linq.Extras
 {
     partial class XEnumerable
     {
+        /// <summary>
+        /// Creates a queue from the elements in the source sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The types of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence containing the elements to put in the queue.</param>
+        /// <returns>A queue containing the same elements as the <c>source</c> sequence.</returns>
+        [Pure]
         public static Queue<TSource> ToQueue<TSource>([NotNull] this IEnumerable<TSource> source)
         {
             source.CheckArgumentNull("source");
             return new Queue<TSource>(source);
         }
 
+        /// <summary>
+        /// Creates a stack from the elements in the source sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The types of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence containing the elements to put in the stack.</param>
+        /// <returns>A stack containing the same elements as the <c>source</c> sequence.</returns>
+        [Pure]
         public static Stack<TSource> ToStack<TSource>([NotNull] this IEnumerable<TSource> source)
         {
             source.CheckArgumentNull("source");
             return new Stack<TSource>(source);
         }
 
-        public static HashSet<TSource> ToHashSet<TSource>([NotNull] this IEnumerable<TSource> source)
-        {
-            return source.ToHashSet(null);
-        }
-
-        public static HashSet<TSource> ToHashSet<TSource>([NotNull] this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
+        /// <summary>
+        /// Creates a hash set from the elements in the source sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The types of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence containing the elements to put in the hash set.</param>
+        /// <param name="comparer">A comparer to test for equality between elements.</param>
+        /// <returns>A hash set containing the same elements as the <c>source</c> sequence.</returns>
+        /// <remarks>Since a hash set cannot contain duplicates, duplicate elements from the <c>source</c> sequence will not be included in the hash set.</remarks>
+        [Pure]
+        public static HashSet<TSource> ToHashSet<TSource>([NotNull] this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer = null)
         {
             source.CheckArgumentNull("source");
             return new HashSet<TSource>(source, comparer);
         }
 
+        /// <summary>
+        /// Creates a linked list from the elements in the source sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The types of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence containing the elements to put in the linked list.</param>
+        /// <returns>A linked list containing the same elements as the <c>source</c> sequence.</returns>
+        [Pure]
         public static LinkedList<TSource> ToLinkedList<TSource>([NotNull] this IEnumerable<TSource> source)
         {
             source.CheckArgumentNull("source");
