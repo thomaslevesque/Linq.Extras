f9ef43e46478d2ce3c055bb95eb5cdd0d8dc8b29
Thomas Levesque <tom103@free.fr>
2014-06-10 17:58:39 +0200

    Added tests and documentation for SequenceEqualBy

---
+++ Linq.Extras.Tests/XEnumerableTests/SequenceEqualByTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Linq.Extras.Tests.XEnumerableTests
+{
+    [TestFixture]
+    class SequenceEqualByTests
+    {
+        [Test]
+        public void SequenceEqualBy_Throws_If_Argument_Is_Null()
+        {
+            var source = XEnumerable.Empty<string>().ForbidEnumeration();
+            var other = XEnumerable.Empty<string>().ForbidEnumeration();
+            TestHelper.AssertThrowsWhenArgumentNull(
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                () => source.SequenceEqualBy(other, s => s.Length, null),
+                "source", "other", "keySelector");
+        }
+
+        [Test]
+        public void SequenceEqualBy_Returns_True_If_Both_Sequences_Are_Empty()
+        {
+            var source = XEnumerable.Empty<string>().ForbidMultipleEnumeration();
+            var other = XEnumerable.Empty<string>().ForbidMultipleEnumeration();
+            source.SequenceEqualBy(other, s => s.Length).Should().BeTrue();
+        }
+
+        [Test]
+        public void SequenceEqualBy_Returns_False_If_Sequences_Have_Different_Lengths()
+        {
+            var source = new[] { "hello", "world" }.ForbidMultipleEnumeration();
+            var other = new[]{ "world" }.ForbidMultipleEnumeration();
+            source.SequenceEqualBy(other, s => s.Length).Should().BeFalse();
+        }
+
+        [Test]
+        public void SequenceEqualBy_Returns_True_If_Sequences_Have_Same_Lengths_And_Same_Keys()
+        {
+            var source = new[] { "hello", "!" }.ForbidMultipleEnumeration();
+            var other = new[] { "world", "!" }.ForbidMultipleEnumeration();
+            source.SequenceEqualBy(other, s => s.Length).Should().BeTrue();
+        }
+    }
+}
--- Linq.Extras/SequenceEqualBy.cs
+++ Linq.Extras/SequenceEqualBy.cs
@@ -8,21 +8,22 @@ namespace Linq.Extras
 {
     partial class XEnumerable
     {
-        [Pure]
-        public static bool SequenceEqualBy<TSource, TKey>(
-            [NotNull] this IEnumerable<TSource> source,
-            [NotNull] IEnumerable<TSource> other,
-            [NotNull] Func<TSource, TKey> keySelector)
-        {
-            return source.SequenceEqualBy(other, keySelector, null);
-        }
-
+        /// <summary>
+        /// Determines whether two sequences are equal by comparing their elements using the specified key and key comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c> and <c>other</c>.</typeparam>
+        /// <typeparam name="TKey">The type of the key used to test for equality between elements.</typeparam>
+        /// <param name="source">The first sequence to compare.</param>
+        /// <param name="other">The second sequence to compare.</param>
+        /// <param name="keySelector">A function that returns the key to use to test for equality between elements.</param>
+        /// <param name="keyComparer">A comparer used to compare the keys.</param>
+        /// <returns>true if the two source sequences are of equal length and their corresponding elements compare equal according to the key and key comparer; otherwise, false.</returns>
         [Pure]
         public static bool SequenceEqualBy<TSource, TKey>(
             [NotNull] this IEnumerable<TSource> source,
             [NotNull] IEnumerable<TSource> other,
             [NotNull] Func<TSource, TKey> keySelector,
-            IEqualityComparer<TKey> keyComparer)
+            IEqualityComparer<TKey> keyComparer = null)
         {
             source.CheckArgumentNull("source");
             other.CheckArgumentNull("other");
