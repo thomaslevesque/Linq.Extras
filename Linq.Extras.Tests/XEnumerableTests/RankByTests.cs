e17afe409d55d7b70c5a408ae588a69e84860ec2
Thomas Levesque <tom103@free.fr>
2014-06-10 17:03:49 +0200

    Added tests and documentation for RankBy/DenseRankBy

---
+++ Linq.Extras.Tests/XEnumerableTests/RankByTests.cs
@@ -0,0 +1,229 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Linq.Extras.Tests.XEnumerableTests
+{
+    [TestFixture]
+    class RankByTests
+    {
+        [Test]
+        public void RankBy_Throws_When_Argument_Is_Null()
+        {
+            var source = XEnumerable.Empty<int>().ForbidEnumeration();
+            TestHelper.AssertThrowsWhenArgumentNull(
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                () => source.RankBy(x => x, (x, r) => x, null),
+                "source", "keySelector", "resultSelector");
+        }
+
+        [Test]
+        public void RankBy_Associates_Item_With_Rank_NoDraws()
+        {
+            var source = new[]
+                         {
+                             new Player("Alice", 42),
+                             new Player("Bob", 25),
+                             new Player("Charlie", 75),
+                             new Player("David", 17),
+                             new Player("Emily", 31)
+                         }.ForbidMultipleEnumeration();
+
+            var result = source.RankBy(p => p.Score, (player, rank) => string.Format("{0}. {1}", rank, player.Name));
+            result.Should().BeEquivalentTo(
+                "1. David",
+                "2. Bob",
+                "3. Emily",
+                "4. Alice",
+                "5. Charlie");
+        }
+
+        [Test]
+        public void RankBy_Associates_Item_With_Rank_WithDraws()
+        {
+            var source = new[]
+                         {
+                             new Player("Alice", 42),
+                             new Player("Bob", 25),
+                             new Player("Charlie", 25),
+                             new Player("David", 17),
+                             new Player("Emily", 42)
+                         }.ForbidMultipleEnumeration();
+
+            var result = source.RankBy(p => p.Score, (player, rank) => string.Format("{0}. {1}", rank, player.Name));
+            result.Should().BeEquivalentTo(
+                "1. David",
+                "2. Bob",
+                "2. Charlie",
+                "4. Alice",
+                "4. Emily");
+        }
+
+        [Test]
+        public void RankByDescending_Throws_When_Argument_Is_Null()
+        {
+            var source = XEnumerable.Empty<int>().ForbidEnumeration();
+            TestHelper.AssertThrowsWhenArgumentNull(
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                () => source.RankByDescending(x => x, (x, r) => x, null),
+                "source", "keySelector", "resultSelector");
+        }
+
+        [Test]
+        public void RankByDescending_Associates_Item_With_Rank_NoDraws()
+        {
+            var source = new[]
+                         {
+                             new Player("Alice", 42),
+                             new Player("Bob", 25),
+                             new Player("Charlie", 75),
+                             new Player("David", 17),
+                             new Player("Emily", 31)
+                         }.ForbidMultipleEnumeration();
+
+            var result = source.RankByDescending(p => p.Score, (player, rank) => string.Format("{0}. {1}", rank, player.Name));
+            result.Should().BeEquivalentTo(
+                "1. Charlie",
+                "2. Alice",
+                "3. Emily",
+                "4. Bob",
+                "5. David");
+        }
+
+        [Test]
+        public void RankByDescending_Associates_Item_With_Rank_WithDraws()
+        {
+            var source = new[]
+                         {
+                             new Player("Alice", 42),
+                             new Player("Bob", 25),
+                             new Player("Charlie", 25),
+                             new Player("David", 17),
+                             new Player("Emily", 42)
+                         }.ForbidMultipleEnumeration();
+
+            var result = source.RankByDescending(p => p.Score, (player, rank) => string.Format("{0}. {1}", rank, player.Name));
+            result.Should().BeEquivalentTo(
+                "1. Alice",
+                "1. Emily",
+                "3. Bob",
+                "3. Charlie",
+                "5. David");
+        }
+
+        [Test]
+        public void DenseRankBy_Throws_When_Argument_Is_Null()
+        {
+            var source = XEnumerable.Empty<int>().ForbidEnumeration();
+            TestHelper.AssertThrowsWhenArgumentNull(
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                () => source.DenseRankBy(x => x, (x, r) => x, null),
+                "source", "keySelector", "resultSelector");
+        }
+
+        [Test]
+        public void DenseRankBy_Associates_Item_With_Rank_NoDraws()
+        {
+            var source = new[]
+                         {
+                             new Player("Alice", 42),
+                             new Player("Bob", 25),
+                             new Player("Charlie", 75),
+                             new Player("David", 17),
+                             new Player("Emily", 31)
+                         }.ForbidMultipleEnumeration();
+
+            var result = source.DenseRankBy(p => p.Score, (player, rank) => string.Format("{0}. {1}", rank, player.Name));
+            result.Should().BeEquivalentTo(
+                "1. David",
+                "2. Bob",
+                "3. Emily",
+                "4. Alice",
+                "5. Charlie");
+        }
+
+        [Test]
+        public void DenseRankBy_Associates_Item_With_Rank_WithDraws()
+        {
+            var source = new[]
+                         {
+                             new Player("Alice", 42),
+                             new Player("Bob", 25),
+                             new Player("Charlie", 25),
+                             new Player("David", 17),
+                             new Player("Emily", 42)
+                         }.ForbidMultipleEnumeration();
+
+            var result = source.DenseRankBy(p => p.Score, (player, rank) => string.Format("{0}. {1}", rank, player.Name));
+            result.Should().BeEquivalentTo(
+                "1. David",
+                "2. Bob",
+                "2. Charlie",
+                "3. Alice",
+                "3. Emily");
+        }
+
+        [Test]
+        public void DenseRankByDescending_Throws_When_Argument_Is_Null()
+        {
+            var source = XEnumerable.Empty<int>().ForbidEnumeration();
+            TestHelper.AssertThrowsWhenArgumentNull(
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                () => source.DenseRankByDescending(x => x, (x, r) => x, null),
+                "source", "keySelector", "resultSelector");
+        }
+
+        [Test]
+        public void DenseRankByDescending_Associates_Item_With_Rank_NoDraws()
+        {
+            var source = new[]
+                         {
+                             new Player("Alice", 42),
+                             new Player("Bob", 25),
+                             new Player("Charlie", 75),
+                             new Player("David", 17),
+                             new Player("Emily", 31)
+                         }.ForbidMultipleEnumeration();
+
+            var result = source.DenseRankByDescending(p => p.Score, (player, rank) => string.Format("{0}. {1}", rank, player.Name));
+            result.Should().BeEquivalentTo(
+                "1. Charlie",
+                "2. Alice",
+                "3. Emily",
+                "4. Bob",
+                "5. David");
+        }
+
+        [Test]
+        public void DenseRankByDescending_Associates_Item_With_Rank_WithDraws()
+        {
+            var source = new[]
+                         {
+                             new Player("Alice", 42),
+                             new Player("Bob", 25),
+                             new Player("Charlie", 25),
+                             new Player("David", 17),
+                             new Player("Emily", 42)
+                         }.ForbidMultipleEnumeration();
+
+            var result = source.DenseRankByDescending(p => p.Score, (player, rank) => string.Format("{0}. {1}", rank, player.Name));
+            result.Should().BeEquivalentTo(
+                "1. Alice",
+                "1. Emily",
+                "2. Bob",
+                "2. Charlie",
+                "3. David");
+        }
+
+        class Player
+        {
+            public Player(string name, int score)
+            {
+                Name = name;
+                Score = score;
+            }
+
+            public string Name { get; private set; }
+            public int Score { get; private set; }
+        }
+    }
+}
--- Linq.Extras/RankBy.cs
+++ Linq.Extras/RankBy.cs
@@ -8,63 +8,89 @@ namespace Linq.Extras
 {
     partial class XEnumerable
     {
+        /// <summary>
+        /// Associates an ascending rank to each element of the input sequence, based on the specified key and key comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <typeparam name="TKey">The type of the key used to compare elements.</typeparam>
+        /// <typeparam name="TResult">The type of the elements in the output sequence.</typeparam>
+        /// <param name="source">The input sequence.</param>
+        /// <param name="keySelector">A function that returns the key for each element.</param>
+        /// <param name="resultSelector">A projection that takes an element and its rank and returns an output element.</param>
+        /// <param name="keyComparer">The comparer used to compare the keys.</param>
+        /// <returns>A sequence of elements associated with their rank using the result selector.</returns>
+        /// <remarks>
+        /// This method produces a sparse ranking, i.e. elements with equal rank produce "holes" in the ranks. In the following example,
+        /// there are two elements with rank 2, so there is no element with rank 3; the rank of the next element is 4.
+        /// <list type="table">
+        ///     <listheader>
+        ///         <term>Rank</term>
+        ///         <description>Value</description>
+        ///     </listheader>
+        ///     <item>
+        ///         <term>1</term>
+        ///         <description>A</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>2</term>
+        ///         <description>B</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>2</term>
+        ///         <description>B</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>4</term>
+        ///         <description>C</description>
+        ///     </item>
+        /// </list>
+        /// </remarks>
         [Pure]
         public static IEnumerable<TResult> RankBy<TSource, TKey, TResult>(
             [NotNull] this IEnumerable<TSource> source,
             [NotNull] Func<TSource, TKey> keySelector,
-            [NotNull] Func<TSource, int, TResult> resultSelector)
+            [NotNull] Func<TSource, int, TResult> resultSelector,
+            IComparer<TKey> keyComparer = null)
         {
             source.CheckArgumentNull("source");
             keySelector.CheckArgumentNull("keySelector");
             resultSelector.CheckArgumentNull("resultSelector");
-            return source.RankByImpl(keySelector, null, false, resultSelector);
+            return source.RankByImpl(keySelector, resultSelector, keyComparer, false);
         }
 
-        [Pure]
-        public static IEnumerable<TResult> RankBy<TSource, TKey, TResult>(
-            [NotNull] this IEnumerable<TSource> source,
-            [NotNull] Func<TSource, TKey> keySelector,
-            IComparer<TKey> keyComparer,
-            [NotNull] Func<TSource, int, TResult> resultSelector)
-        {
-            source.CheckArgumentNull("source");
-            keySelector.CheckArgumentNull("keySelector");
-            resultSelector.CheckArgumentNull("resultSelector");
-            return source.RankByImpl(keySelector, keyComparer, false, resultSelector);
-        }
-
-        [Pure]
-        public static IEnumerable<TResult> RankByDescending<TSource, TKey, TResult>(
-            [NotNull] this IEnumerable<TSource> source,
-            [NotNull] Func<TSource, TKey> keySelector,
-            IComparer<TKey> keyComparer,
-            [NotNull] Func<TSource, int, TResult> resultSelector)
-        {
-            source.CheckArgumentNull("source");
-            keySelector.CheckArgumentNull("keySelector");
-            resultSelector.CheckArgumentNull("resultSelector");
-            return source.RankByImpl(keySelector, keyComparer, true, resultSelector);
-        }
-
+        /// <summary>
+        /// Associates a descending rank to each element of the input sequence, based on the specified key and key comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <typeparam name="TKey">The type of the key used to compare elements.</typeparam>
+        /// <typeparam name="TResult">The type of the elements in the output sequence.</typeparam>
+        /// <param name="source">The input sequence.</param>
+        /// <param name="keySelector">A function that returns the key for each element.</param>
+        /// <param name="resultSelector">A projection that takes an element and its rank and returns an output element.</param>
+        /// <param name="keyComparer">The comparer used to compare the keys.</param>
+        /// <returns>A sequence of elements associated with their rank using the result selector.</returns>
+        /// <remarks>
+        /// This method produces a sparse ranking, i.e. elements with equal rank produce "holes" in the ranks. See <see cref="RankBy{TSource,TKey,TResult}"/> for an example.
+        /// </remarks>
         [Pure]
         public static IEnumerable<TResult> RankByDescending<TSource, TKey, TResult>(
             [NotNull] this IEnumerable<TSource> source,
             [NotNull] Func<TSource, TKey> keySelector,
-            [NotNull] Func<TSource, int, TResult> resultSelector)
+            [NotNull] Func<TSource, int, TResult> resultSelector,
+            IComparer<TKey> keyComparer = null)
         {
             source.CheckArgumentNull("source");
             keySelector.CheckArgumentNull("keySelector");
             resultSelector.CheckArgumentNull("resultSelector");
-            return source.RankByImpl(keySelector, null, true, resultSelector);
+            return source.RankByImpl(keySelector, resultSelector, keyComparer, true);
         }
 
-        [Pure]
         private static IEnumerable<TResult> RankByImpl<TSource, TKey, TResult>(
             this IEnumerable<TSource> source,
             Func<TSource, TKey> keySelector,
+            Func<TSource, int, TResult> resultSelector,
             IComparer<TKey> keyComparer,
-            bool descending,
-            Func<TSource, int, TResult> resultSelector)
+            bool descending)
         {
             keyComparer = keyComparer ?? Comparer<TKey>.Default;
 
@@ -86,63 +112,90 @@ namespace Linq.Extras
             }
         }
 
+        /// <summary>
+        /// Associates an ascending dense rank to each element of the input sequence, based on the specified key and key comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <typeparam name="TKey">The type of the key used to compare elements.</typeparam>
+        /// <typeparam name="TResult">The type of the elements in the output sequence.</typeparam>
+        /// <param name="source">The input sequence.</param>
+        /// <param name="keySelector">A function that returns the key for each element.</param>
+        /// <param name="resultSelector">A projection that takes an element and its rank and returns an output element.</param>
+        /// <param name="keyComparer">The comparer used to compare the keys.</param>
+        /// <returns>A sequence of elements associated with their rank using the result selector.</returns>
+        /// <remarks>
+        /// This method produces a dense ranking, i.e. elements with equal rank produce no "holes" in the ranks. In the following example,
+        /// there are two elements with rank 2, and the rank of the next element is 3.
+        /// <list type="table">
+        ///     <listheader>
+        ///         <term>Dense rank</term>
+        ///         <description>Value</description>
+        ///     </listheader>
+        ///     <item>
+        ///         <term>1</term>
+        ///         <description>A</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>2</term>
+        ///         <description>B</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>2</term>
+        ///         <description>B</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>3</term>
+        ///         <description>C</description>
+        ///     </item>
+        /// </list>
+        /// </remarks>
         [Pure]
         public static IEnumerable<TResult> DenseRankBy<TSource, TKey, TResult>(
             [NotNull] this IEnumerable<TSource> source,
             [NotNull] Func<TSource, TKey> keySelector,
-            [NotNull] Func<TSource, int, TResult> resultSelector)
+            [NotNull] Func<TSource, int, TResult> resultSelector,
+            IComparer<TKey> keyComparer = null)
         {
             source.CheckArgumentNull("source");
             keySelector.CheckArgumentNull("keySelector");
             resultSelector.CheckArgumentNull("resultSelector");
-            return source.DenseRankByImpl(keySelector, null, false, resultSelector);
+            return source.DenseRankByImpl(keySelector, resultSelector, keyComparer, false);
         }
 
-        [Pure]
-        public static IEnumerable<TResult> DenseRankBy<TSource, TKey, TResult>(
-            [NotNull] this IEnumerable<TSource> source,
-            [NotNull] Func<TSource, TKey> keySelector,
-            IComparer<TKey> keyComparer,
-            [NotNull] Func<TSource, int, TResult> resultSelector)
-        {
-            source.CheckArgumentNull("source");
-            keySelector.CheckArgumentNull("keySelector");
-            resultSelector.CheckArgumentNull("resultSelector");
-            return source.DenseRankByImpl(keySelector, keyComparer, false, resultSelector);
-        }
-
-        [Pure]
-        public static IEnumerable<TResult> DenseRankByDescending<TSource, TKey, TResult>(
-            [NotNull] this IEnumerable<TSource> source,
-            [NotNull] Func<TSource, TKey> keySelector,
-            IComparer<TKey> keyComparer,
-            [NotNull] Func<TSource, int, TResult> resultSelector)
-        {
-            source.CheckArgumentNull("source");
-            keySelector.CheckArgumentNull("keySelector");
-            resultSelector.CheckArgumentNull("resultSelector");
-            return source.DenseRankByImpl(keySelector, keyComparer, true, resultSelector);
-        }
-
+        /// <summary>
+        /// Associates a descending dense rank to each element of the input sequence, based on the specified key and key comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <typeparam name="TKey">The type of the key used to compare elements.</typeparam>
+        /// <typeparam name="TResult">The type of the elements in the output sequence.</typeparam>
+        /// <param name="source">The input sequence.</param>
+        /// <param name="keySelector">A function that returns the key for each element.</param>
+        /// <param name="resultSelector">A projection that takes an element and its rank and returns an output element.</param>
+        /// <param name="keyComparer">The comparer used to compare the keys.</param>
+        /// <returns>A sequence of elements associated with their rank using the result selector.</returns>
+        /// <remarks>
+        /// This method produces a dense ranking, i.e. elements with equal rank produce no "holes" in the ranks. See <see cref="DenseRankBy{TSource,TKey,TResult}"/> for an example.
+        /// </remarks>
         [Pure]
         public static IEnumerable<TResult> DenseRankByDescending<TSource, TKey, TResult>(
             [NotNull] this IEnumerable<TSource> source,
             [NotNull] Func<TSource, TKey> keySelector,
-            [NotNull] Func<TSource, int, TResult> resultSelector)
+            [NotNull] Func<TSource, int, TResult> resultSelector,
+            IComparer<TKey> keyComparer = null)
         {
             source.CheckArgumentNull("source");
             keySelector.CheckArgumentNull("keySelector");
             resultSelector.CheckArgumentNull("resultSelector");
-            return source.DenseRankByImpl(keySelector, null, true, resultSelector);
+            return source.DenseRankByImpl(keySelector, resultSelector, keyComparer, true);
         }
 
         [Pure]
         private static IEnumerable<TResult> DenseRankByImpl<TSource, TKey, TResult>(
             this IEnumerable<TSource> source,
             Func<TSource, TKey> keySelector,
+            Func<TSource, int, TResult> resultSelector,
             IComparer<TKey> keyComparer,
-            bool descending,
-            Func<TSource, int, TResult> resultSelector)
+            bool descending)
         {
             keyComparer = keyComparer ?? Comparer<TKey>.Default;
 
