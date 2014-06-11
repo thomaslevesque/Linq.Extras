2ce63adce6e69c0e4e6b24b7cc64c5660385310c
Thomas Levesque <tom103@free.fr>
2014-06-11 22:36:14 +0200

    Added tests and documentation for Unit and Empty

---
+++ Linq.Extras.Tests/XEnumerableTests/MiscTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Linq.Extras.Tests.XEnumerableTests
+{
+    [TestFixture]
+    class MiscTests
+    {
+        [Test]
+        public void Unit_Returns_Sequence_With_Single_Element()
+        {
+            var result = XEnumerable.Unit(42);
+            (result as ICollection).Should().BeNull("Sequence should not be a collection");
+            result.Should().HaveCount(1);
+        }
+
+        [Test]
+        public void Empty_Returns_Empty_Sequence()
+        {
+            var result = XEnumerable.Empty<int>();
+            (result as ICollection).Should().BeNull("Sequence should not be a collection");
+            result.Should().BeEmpty();
+        }
+    }
+}
--- Linq.Extras/XEnumerable.cs
+++ Linq.Extras/XEnumerable.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 
 namespace Linq.Extras
 {
     /// <summary>
-    /// Provides extension and helper methods that work on sequences and collections.
+    /// Provides extension and helper methods to work with sequences.
     /// </summary>
     [PublicAPI]
     public static partial class XEnumerable
     {
+        /// <summary>
+        /// Produces a sequence containing a single element.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the element in the sequence.</typeparam>
+        /// <param name="item">The element to include in the sequence.</param>
+        /// <returns>A sequence containing the specified element.</returns>
+        /// <remarks>The output sequence is lazily evaluated.</remarks>
         [Pure]
         public static IEnumerable<TSource> Unit<TSource>(TSource item)
         {
             yield return item;
         }
 
+        /// <summary>
+        /// Produces an empty sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The element typeof the sequence.</typeparam>
+        /// <returns>An empty sequence.</returns>
+        /// <remarks>
+        /// Unlike <see cref="Enumerable.Empty{TResult}"/>, this method returns a lazily evaluated sequence; <see cref="Enumerable.Empty{TResult}"/> returns an empty array.</remarks>
         [Pure]
         public static IEnumerable<TSource> Empty<TSource>()
         {
