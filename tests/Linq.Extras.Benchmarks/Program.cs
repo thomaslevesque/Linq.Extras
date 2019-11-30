937adef5fe91daf98aa18c2a4e2b060851538d42
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-11-30 15:17:25 +0100

    Initial benchmark for WithIndex

---
+++ tests/Linq.Extras.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linq.Extras.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tests/Linq.Extras.Benchmarks/WithIndexOnEnumerableBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq.Extras.Benchmarks
+{
+    public class WithIndexOnEnumerableBenchmark
+    {
+        private IEnumerable<int> _enumerable;
+
+        [Params(100, 1000, 10000)]
+        public int N { get; set; }
+
+        [GlobalSetup]
+        public void Prepare()
+        {
+            _enumerable = Enumerable.Range(0, N);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void ForEachLoop()
+        {
+            var sum = 0;
+            int index = 0;
+            foreach (var i in _enumerable)
+            {
+                sum += index + i;
+                index++;
+            }
+        }
+
+        [Benchmark]
+        public void WithIndex()
+        {
+            var sum = 0;
+            foreach (var (item, index) in _enumerable.WithIndex())
+                sum += item + index;
+        }
+    }
+}
