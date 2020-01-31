83fe16c57da9b720448651377c5eb7607d1a5c9a
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2020-01-31 16:13:10 +0100

    [WIP] Add Min/MaxOrDefault and variants

--- src/Linq.Extras/MinMax.cs
+++ src/Linq.Extras/MinMax.cs
@@ -26,6 +26,42 @@ namespace Linq.Extras
         }
 
         /// <summary>
+        /// Returns the maximum element of the sequence according to the specified comparer,
+        /// or a default value if the sequence is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to return the maximum element from.</param>
+        /// <param name="comparer">The comparer used to compare elements.</param>
+        /// <param name="defaultValue">The default value to return if the sequence is empty.</param>
+        /// <returns>The maximum element according to the specified comparer.</returns>
+        [Pure]
+        public static TSource MaxOrDefault<TSource>(
+            [NotNull] this IEnumerable<TSource> source,
+            [NotNull] IComparer<TSource> comparer,
+            TSource defaultValue = default)
+        {
+            source.CheckArgumentNull(nameof(source));
+            comparer.CheckArgumentNull(nameof(comparer));
+            return source.ExtremeOrDefault(comparer, 1, defaultValue);
+        }
+
+        /// <summary>
+        /// Returns the maximum element of the sequence, or a default value if the sequence is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to return the maximum element from.</param>
+        /// <param name="defaultValue">The default value to return if the sequence is empty.</param>
+        /// <returns>The maximum element according to the specified comparer.</returns>
+        [Pure]
+        public static TSource MaxOrDefault<TSource>(
+            [NotNull] this IEnumerable<TSource> source,
+            TSource defaultValue = default)
+        {
+            source.CheckArgumentNull(nameof(source));
+            return source.ExtremeOrDefault(null, 1, defaultValue);
+        }
+
+        /// <summary>
         /// Returns the minimum element of the sequence according to the specified comparer.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
@@ -42,10 +78,46 @@ namespace Linq.Extras
             return source.Extreme(comparer, -1);
         }
 
+        /// <summary>
+        /// Returns the minimum element of the sequence according to the specified comparer,
+        /// or a default value if the sequence is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to return the minimum element from.</param>
+        /// <param name="comparer">The comparer used to compare elements.</param>
+        /// <param name="defaultValue">The default value to return if the sequence is empty.</param>
+        /// <returns>The minimum element according to the specified comparer.</returns>
         [Pure]
-        private static TSource Extreme<TSource>(this IEnumerable<TSource> source, IComparer<TSource> comparer, int sign)
+        public static TSource MinOrDefault<TSource>(
+            [NotNull] this IEnumerable<TSource> source,
+            [NotNull] IComparer<TSource> comparer,
+            TSource defaultValue = default)
         {
-            comparer = comparer ?? Comparer<TSource>.Default;
+            source.CheckArgumentNull(nameof(source));
+            comparer.CheckArgumentNull(nameof(comparer));
+            return source.ExtremeOrDefault(comparer, -1, defaultValue);
+        }
+
+        /// <summary>
+        /// Returns the minimum element of the sequence, or a default value if the sequence is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <param name="source">The sequence to return the minimum element from.</param>
+        /// <param name="defaultValue">The default value to return if the sequence is empty.</param>
+        /// <returns>The minimum element according to the specified comparer.</returns>
+        [Pure]
+        public static TSource MinOrDefault<TSource>(
+            [NotNull] this IEnumerable<TSource> source,
+            TSource defaultValue = default)
+        {
+            source.CheckArgumentNull(nameof(source));
+            return source.ExtremeOrDefault(null, -1, defaultValue);
+        }
+
+        [Pure]
+        private static TSource Extreme<TSource>(this IEnumerable<TSource> source, IComparer<TSource>? comparer, int sign)
+        {
+            comparer ??= Comparer<TSource>.Default;
             TSource extreme = default!;
             bool first = true;
             foreach (var item in source)
@@ -63,6 +135,31 @@ namespace Linq.Extras
 
             if (first)
                 throw EmptySequenceException();
+
+            return extreme;
+        }
+
+        [Pure]
+        private static TSource ExtremeOrDefault<TSource>(this IEnumerable<TSource> source, IComparer<TSource>? comparer, int sign, TSource defaultValue)
+        {
+            comparer ??= Comparer<TSource>.Default;
+            TSource extreme = default!;
+            bool first = true;
+            foreach (var item in source)
+            {
+                int compare = 0;
+                if (!first)
+                    compare = comparer.Compare(item, extreme);
+
+                if (Math.Sign(compare) == sign || first)
+                {
+                    extreme = item;
+                }
+                first = false;
+            }
+
+            if (first)
+                return defaultValue;
 
             return extreme;
         }
@@ -94,6 +191,30 @@ namespace Linq.Extras
         }
 
         /// <summary>
+        /// Returns the element of the sequence that has the maximum value for the specified key,
+        /// or a default value if the sequence is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <typeparam name="TKey">The type of the key used to compare elements.</typeparam>
+        /// <param name="source">The sequence to return an element from.</param>
+        /// <param name="keySelector">A delegate that returns the key used to compare elements.</param>
+        /// <param name="keyComparer">A comparer to compare the keys.</param>
+        /// <param name="defaultValue">The default value to return if the sequence is empty.</param>
+        /// <returns>The element of <c>source</c> that has the maximum value for the specified key.</returns>
+        [Pure]
+        public static TSource MaxByOrDefault<TSource, TKey>(
+            [NotNull] this IEnumerable<TSource> source,
+            [NotNull] Func<TSource, TKey> keySelector,
+            IComparer<TKey>? keyComparer = null,
+            TSource defaultValue = default)
+        {
+            source.CheckArgumentNull(nameof(source));
+            keySelector.CheckArgumentNull(nameof(keySelector));
+            var comparer = XComparer.By(keySelector, keyComparer);
+            return source.MaxOrDefault(comparer, defaultValue);
+        }
+
+        /// <summary>
         /// Returns the element of the sequence that has the minimum value for the specified key.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
@@ -112,6 +233,30 @@ namespace Linq.Extras
             keySelector.CheckArgumentNull(nameof(keySelector));
             var comparer = XComparer.By(keySelector, keyComparer);
             return source.Min(comparer);
+        }
+
+        /// <summary>
+        /// Returns the element of the sequence that has the minimum value for the specified key,
+        /// or a default value if the sequence is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <c>source</c>.</typeparam>
+        /// <typeparam name="TKey">The type of the key used to compare elements.</typeparam>
+        /// <param name="source">The sequence to return an element from.</param>
+        /// <param name="keySelector">A delegate that returns the key used to compare elements.</param>
+        /// <param name="keyComparer">A comparer to compare the keys.</param>
+        /// <param name="defaultValue">The default value to return if the sequence is empty.</param>
+        /// <returns>The element of <c>source</c> that has the minimum value for the specified key.</returns>
+        [Pure]
+        public static TSource MinByOrDefault<TSource, TKey>(
+            [NotNull] this IEnumerable<TSource> source,
+            [NotNull] Func<TSource, TKey> keySelector,
+            IComparer<TKey>? keyComparer = null,
+            TSource defaultValue = default)
+        {
+            source.CheckArgumentNull(nameof(source));
+            keySelector.CheckArgumentNull(nameof(keySelector));
+            var comparer = XComparer.By(keySelector, keyComparer);
+            return source.MinOrDefault(comparer, defaultValue);
         }
     }
 }
